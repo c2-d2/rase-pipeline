6c0122a52a9f1a15790525046b122d99b950e80e
Karel Břinda <karel.brinda@gmail.com>
2018-05-15 20:47:13 -0400

    Add a script for testing the environment

---
+++ scripts/test_environments.sh
@@ -0,0 +1,25 @@
+#! /usr/bin/env bash
+
+
+set -e
+set -o pipefail
+
+# $1 - file
+# $2 - expected error code
+test () {
+    file="$1"
+    experr="$2"
+    echo "Testing $x"
+    tmp=$(mktemp)
+    ((./$file 2>&1)> "$tmp" || (code=$? && exit $(($code - $experr))) ) || ( echo "...failed ($code)" && cat "$tmp"  && exit 1)
+}
+
+# argparse should return exit status 2
+for x in *.py; do
+    test "$x" 2
+done
+
+# R should exit with 1
+for x in *.R; do
+    test "$x" 1
+done
