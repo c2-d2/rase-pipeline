81872331f685e97ca71eea67dd337775c33aad38
Karel Břinda <karel.brinda@gmail.com>
2018-05-16 10:09:33 -0400

    Run tests for the environment in parallel

--- scripts/test_environments.sh
+++ scripts/test_environments.sh
@@ -18,10 +18,13 @@ DIR=`dirname $0`
 
 # argparse should return exit status 2
 for x in "$DIR"/*.py; do
-    test "$x" 2
+    test "$x" 2 &
 done
 
 # R should exit with 1
 for x in *.R; do
-    test "$x" 1
+    test "$x" 1 &
 done
+
+wait
+
